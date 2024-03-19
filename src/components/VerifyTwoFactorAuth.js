dcc272b82daaab318301a1476fd833d26f2ea573
ABHISHEK NANDKUMAR INGLE <abhishekingle662@gmail.com>
2024-03-19 14:42:54 -0400

    UI changes

--- src/components/VerifyTwoFactorAuth.js
+++ src/components/VerifyTwoFactorAuth.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { verifyOtpSuccess, verifyOtpFailure } from '../actions/authActions';
+import 'typeface-roboto';
 
 
 const VerifyTwoFactorAuth = () => {
@@ -41,36 +42,68 @@ const VerifyTwoFactorAuth = () => {
     container: {
       display: 'flex',
       flexDirection: 'column',
-      alignItems: 'center',
-      marginTop: '20px',
+      justifyContent: 'center', // Centers content vertically in the container
+      alignItems: 'center', // Centers content horizontally in the container
+      position: 'absolute', // Use absolute positioning
+      top: '50%', // Position halfway down the parent
+      left: '50%', // Position halfway across the parent
+      transform: 'translate(-50%, -50%)', // This will center the container
+      padding: '20px',
+      backgroundColor: '#fff',
+      borderRadius: '10px',
+      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+      maxWidth: '400px',
+      width: '80%', // You can adjust this as necessary
+      boxSizing: 'border-box',
+      fontFamily: '"Roboto", sans-serif', // Ensure the font family is applied
+    },
+    input: {
+      margin: '10px 0',
+      padding: '10px',
+      borderRadius: '5px',
+      border: '1px solid #ccc',
+      width: '100%', // Make the input full width of the container
     },
     button: {
       marginTop: '10px',
       padding: '10px 20px',
       borderRadius: '5px',
-      border: '1px solid #ccc',
+      border: 'none',
       cursor: 'pointer',
-      backgroundColor: '#f0f0f0',
+      backgroundColor: '#0056b3',
+      color: '#fff',
       fontWeight: 'bold',
-    },
-    image: {
-      marginTop: '20px',
+      textTransform: 'uppercase',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+      width: '100%', // Make the button full width of the container
+      fontFamily: '"Roboto", sans-serif',
     },
     title: {
       color: '#333',
+      textAlign: 'center',
+      margin: '10px 0',
+      fontWeight: 'bold',
+      fontFamily: '"Roboto", sans-serif',
     }
   };
 
   return (
     <div style={styles.container}>
-      <h2>Verify Two-Factor Authentication</h2>
+      <h2 style={styles.title}>Verify Two-Factor Authentication</h2>
       <input
+        style={styles.input}
         type="text"
         value={otp}
         onChange={(e) => setOtp(e.target.value)}
         placeholder="Enter your OTP"
       />
-      <button onClick={verifyOtp}>Verify</button>
+      <button
+        style={styles.button}
+        onClick={verifyOtp}
+        className="buttonHoverEffect" // Apply the same hover effect as defined in the external CSS
+      >
+        Verify
+      </button>
     </div>
   );
 };
--- src/components/searchResults/searchResults.jsx
+++ src/components/searchResults/searchResults.jsx
@@ -1,4 +1,3 @@
-import "./searchResults.scss"
 import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useUserActions } from '../../context/UserActionsContext';
@@ -9,19 +8,28 @@ function useQuery() {
 
 const SearchResultsPage = () => {
   const query = useQuery().get('query');
-  const [searchResults, setSearchResults] = useState([]);
-  
-  const { handleSearch } = useUserActions();
+  const [searchResultsUser, setSearchResultsUser] = useState([]);
+  const [searchResultsPost, setSearchResultsPost] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const { handleSearch, handleAddFriend, handleRemoveFriend, 
+    handleBlockUser, handleUnblockUser } = useUserActions();
+
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchSearchResults = async () => {
       try {
+        setLoading(true);
         const results = await handleSearch(query);
-        setSearchResults(results);
+        setSearchResultsUser(results.users);
+        setSearchResultsPost(results.posts);
       } catch (error) {
         console.error('Failed to fetch search results:', error);
-        setSearchResults([]);
+        setSearchResultsUser([]);
+        setSearchResultsPost([]);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -30,29 +38,125 @@ const SearchResultsPage = () => {
     }
   }, [query]);
 
-  return (
-    <div class="user-list">
-        {searchResults.map((user, index) => (
-            <div key={index}  class="user-item">
-            <p>{user.firstname} {user.lastname} (@{user.username})</p>
-            {/* <button onClick={() => removeFriend(user.id)}>Remove Friend</button>
-            <button onClick={() => blockUser(user.id)}>Block</button>
-            <button onClick={() => navigate(`/user/${user.username}`)}>View Profile</button>
-              {user.requestSent === "NONE" && <button onClick={() => onAddFriend(user.username)}>Add Friend</button>}
-              {user.requestSent === "PENDING" && <button disabled>Pending</button>}
-              {user.requestSent === "ACCEPTED" && <button disabled>Friends</button>}
-              {user.requestSent === "BLOCKED" && <button onClick={() => unblockUser(user.id)}>Unblock</button>} */}
-            <button class="remove-friend">Remove Friend</button>
-            <button class="block">Block</button>
-            <button class="view-profile" onClick={() => navigate(`/user/${user.username}`)}>View Profile</button>
-              {user.requestSent === "NONE" && <button>Add Friend</button>}
-              {user.requestSent === "PENDING" && <button disabled>Pending</button>}
-              {user.requestSent === "ACCEPTED" && <button disabled>Friends</button>}
-              {user.requestSent === "BLOCKED" && <button>Unblock</button>}
-            </div>
-        ))}
-     </div>
+  // ADD FRIEND
+  const onAddFriend = async (addFriendUsername) => {
+    const success = await handleAddFriend(addFriendUsername);
 
+    if (success) {
+      alert('Friend request sent successfully!');
+    } else {
+      alert('Failed to send friend request.');
+    }
+  };
+
+  // REMOVE FRIENDS
+  const removeFriend = async (friendId) => {
+    const success = await handleRemoveFriend(friendId);
+    if (success) {
+      alert('Friend removed successfully');
+      // Update friends list accordingly
+    } else {
+      alert('Failed to remove friend');
+    }
+  };
+
+  // BLOCK USER
+  const blockUser = async (userId) => {
+    const success = await handleBlockUser(userId);
+    if (success) {
+      alert('User blocked successfully');
+    } else {
+      alert('Failed to block user');
+    }
+  };
+
+  // UNBLOCK USER
+  const unblockUser = async (userId) => {
+    const success = await handleUnblockUser(userId);
+    if (success) {
+      alert('User unblocked successfully');
+    } else {
+      alert('Failed to unblock user');
+    }
+  };
+
+  return (
+    <div>
+      <div className="user-list">
+        <h1>Users</h1>
+        {loading && <p>Loading users...</p>}
+        {!loading && searchResultsUser && searchResultsUser.length === 0 && <p>No users found.</p>}
+        {!loading && searchResultsUser && searchResultsUser.map((user, index) => (
+          <div key={index} className="user-item card">
+          <p>{user.firstname} {user.lastname} (@{user.username})</p>
+          <button className="remove-friend">Remove Friend</button>
+          <button className="block">Block</button>
+          <button className="view-profile">View Profile</button>
+          {user.requestSent === "NONE" && <button className="add-friend" onClick={() => onAddFriend(user.username)}>Add Friend</button>}
+          {user.requestSent === "PENDING" && <button className="pending" disabled>Pending</button>}
+          {user.requestSent === "ACCEPTED" && <button className="friends" disabled>Friends</button>}
+          {user.requestSent === "BLOCKED" && <button className="unblock" onClick={() => unblockUser(user.id)}>Unblock</button>}
+        </div>
+        
+        ))}
+        {!loading && searchResultsUser.length === 0 && (
+          <div className="user-item card">
+            <p>User Placeholder</p>
+            <button className="remove-friend" disabled>Remove Friend</button>
+            <button className="block" disabled>Block</button>
+            <button className="view-profile" disabled>View Profile</button>
+            <button disabled>Add Friend</button>
+          </div>
+        )}
+      </div>
+  
+      <div>
+        <h1>Posts</h1>
+        {loading && <p>Loading posts...</p>}
+        {!loading && searchResultsPost && searchResultsPost.length === 0 && <p>No posts found.</p>}
+        {!loading && searchResultsPost && searchResultsPost.map((post, index) => (
+          <div key={index} className="post-item card">
+            <h3 style={{ margin: '0 0 10px 0' }}>{post.caption}</h3>
+            <p style={{ margin: '0 0 10px 0' }}>{post.post}</p>
+            {post.imageUrl ? (
+              <img src={post.imageUrl} alt="Post" className="post-image" />
+            ) : (
+              <img src="https://via.placeholder.com/150" alt="Placeholder Image" className="post-image" />
+            )}
+            {post.videoUrl ? (
+              <video controls className="post-video">
+                <source src={post.videoUrl} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            ) : (
+              <video controls className="post-video">
+                <source src="https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            )}
+            {post.createdAt && (
+              <div style={{ color: '#777', fontSize: '0.8rem', marginTop: '10px' }}>
+                Posted on: {new Date(post.createdAt).toLocaleString()}
+              </div>
+            )}
+          </div>
+        ))}
+        {!loading && searchResultsPost.length === 0 && (
+          <div className="post-item card">
+            <h3>Post Title Placeholder</h3>
+            <p>Post Content Placeholder</p>
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image" className="post-image" />
+            <video controls className="post-video">
+              <source src="https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            <div style={{ color: '#777', fontSize: '0.8rem', marginTop: '10px' }}>
+              Posted on: {new Date().toLocaleString()}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
